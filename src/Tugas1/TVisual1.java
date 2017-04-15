c4a7a3a95943eef17151ce22f4addfb7393e0c31
ahmad99 <ataufik009@gmail.com>
2017-04-15 22:27:27 +0800

    POSTTEST

---
+++ src/Tugas1/TVisual1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas1;
+
+/**
+ *
+ * @author Acer
+ */
+public class TVisual1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new jftvisual().setVisible(true);
+            }
+        });
+    }
+    
+}
